6238ce6c7f6e5a34f06cf2cc89dfbbaaf73e914e
M.Huzei <mhuzei@acm.org>
2016-06-22 00:13:40 +0300

    new version for new device

--- astroprog.c
+++ astroprog.c
@@ -1,19 +1,12 @@
 /*******************************************************
-This program was created by the CodeWizardAVR V3.26 
-Automatic Program Generator
-© Copyright 1998-2016 Pavel Haiduc, HP InfoTech s.r.l.
-http://www.hpinfotech.com
-
-Project : astroprog
-Version : 0.1
-Date    : 09.05.2016
-Author  : M. Huzei
+Project : tst
+Version : 1
+Date    : 10.05.2016
+Author  : M.Huzei
 Company : MNU
-Comments: 
-
 
 Chip type               : ATtiny2313A
-AVR Core Clock frequency: 8,000000 MHz
+AVR Core Clock frequency: 4,000000 MHz
 Memory model            : Tiny
 External RAM size       : 0
 Data Stack size         : 32
@@ -22,148 +15,144 @@ Data Stack size         : 32
 #include <io.h>
 #include <tiny2313.h>
 #include <delay.h>
+
 // Declare your global variables here
+
+unsigned char rol(unsigned char rab){
+	unsigned char temp1, temp2;
+	temp1=rab<<1;
+	temp2=temp1>>4;
+	return temp1|temp2;
+};
 
 void main(void)
 {
-// Declare your local variables here
-unsigned char rab=0b00000001, pos=0, mark, step_r, step_l, i;
+	// Declare your local variables here
+	unsigned char rab=0b00000001;
+	int pos=1,  mark, i;
+	// Crystal Oscillator division factor: 1
+	#pragma optsize-
+	CLKPR=(1<<CLKPCE);
+	CLKPR=(0<<CLKPCE) | (0<<CLKPS3) | (0<<CLKPS2) | (0<<CLKPS1) | (0<<CLKPS0);
+	#ifdef _OPTIMIZE_SIZE_
+	#pragma optsize+
+	#endif
 
-// Crystal Oscillator division factor: 1
-#pragma optsize-
-CLKPR=(1<<CLKPCE);
-CLKPR=(0<<CLKPCE) | (0<<CLKPS3) | (0<<CLKPS2) | (0<<CLKPS1) | (0<<CLKPS0);
-#ifdef _OPTIMIZE_SIZE_
-#pragma optsize+
-#endif
+	// Input/Output Ports initialization
+	// Port A initialization
+	// Function: Bit2=In Bit1=In Bit0=In
+	DDRA=(0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+	// State: Bit2=T Bit1=T Bit0=T
+	PORTA=(0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
 
-// Input/Output Ports initialization
-// Port A initialization
-// Function: Bit2=In Bit1=In Bit0=In 
-DDRA=(0<<DDA2) | (0<<DDA1) | (0<<DDA0);
-// State: Bit2=T Bit1=T Bit0=T 
-PORTA=(0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+	// Port B initialization
+	// Function: Bit7=Out Bit6=Out Bit5=Out Bit4=Out Bit3=Out Bit2=Out Bit1=Out Bit0=Out
+	DDRB=(1<<DDB7) | (1<<DDB6) | (1<<DDB5) | (1<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1) | (1<<DDB0);
+	// State: Bit7=0 Bit6=0 Bit5=0 Bit4=0 Bit3=0 Bit2=0 Bit1=0 Bit0=0
+	PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
 
-// Port B initialization
-// Function: Bit7=Out Bit6=Out Bit5=Out Bit4=Out Bit3=Out Bit2=Out Bit1=Out Bit0=Out 
-DDRB=(1<<DDB7) | (1<<DDB6) | (1<<DDB5) | (1<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1) | (1<<DDB0);
-// State: Bit7=0 Bit6=0 Bit5=0 Bit4=0 Bit3=0 Bit2=0 Bit1=0 Bit0=0 
-PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+	// Port D initialization
+	// Function: Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+	DDRD=(0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+	// State: Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+	PORTD=(1<<PORTD6) | (1<<PORTD5) | (1<<PORTD4) | (1<<PORTD3) | (1<<PORTD2) | (1<<PORTD1) | (1<<PORTD0);
+	// Timer/Counter 0 initialization
+	// Clock source: System Clock
+	// Clock value: Timer 0 Stopped
+	// Mode: Normal top=0xFF
+	// OC0A output: Disconnected
+	// OC0B output: Disconnected
+	TCCR0A=(0<<COM0A1) | (0<<COM0A0) | (0<<COM0B1) | (0<<COM0B0) | (0<<WGM01) | (0<<WGM00);
+	TCCR0B=(0<<WGM02) | (0<<CS02) | (0<<CS01) | (0<<CS00);
+	TCNT0=0x00;
+	OCR0A=0x00;
+	OCR0B=0x00;
 
-// Port D initialization
-// Function: Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
-DDRD=(0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
-// State: Bit6=P Bit5=P Bit4=P Bit3=P Bit2=P Bit1=P Bit0=P 
-PORTD=(1<<PORTD6) | (1<<PORTD5) | (1<<PORTD4) | (1<<PORTD3) | (1<<PORTD2) | (1<<PORTD1) | (1<<PORTD0);
+	// Timer/Counter 1 initialization
+	// Clock source: System Clock
+	// Clock value: Timer1 Stopped
+	// Mode: Normal top=0xFFFF
+	// OC1A output: Disconnected
+	// OC1B output: Disconnected
+	// Noise Canceler: Off
+	// Input Capture on Falling Edge
+	// Timer1 Overflow Interrupt: Off
+	// Input Capture Interrupt: Off
+	// Compare A Match Interrupt: Off
+	// Compare B Match Interrupt: Off
+	TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
+	TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
+	TCNT1H=0x00;
+	TCNT1L=0x00;
+	ICR1H=0x00;
+	ICR1L=0x00;
+	OCR1AH=0x00;
+	OCR1AL=0x00;
+	OCR1BH=0x00;
+	OCR1BL=0x00;
 
-// Timer/Counter 0 initialization
-// Clock source: System Clock
-// Clock value: Timer 0 Stopped
-// Mode: Normal top=0xFF
-// OC0A output: Disconnected
-// OC0B output: Disconnected
-TCCR0A=(0<<COM0A1) | (0<<COM0A0) | (0<<COM0B1) | (0<<COM0B0) | (0<<WGM01) | (0<<WGM00);
-TCCR0B=(0<<WGM02) | (0<<CS02) | (0<<CS01) | (0<<CS00);
-TCNT0=0x00;
-OCR0A=0x00;
-OCR0B=0x00;
+	// Timer(s)/Counter(s) Interrupt(s) initialization
+	TIMSK=(0<<TOIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<ICIE1) | (0<<OCIE0B) | (0<<TOIE0) | (0<<OCIE0A);
 
-// Timer/Counter 1 initialization
-// Clock source: System Clock
-// Clock value: Timer1 Stopped
-// Mode: Normal top=0xFFFF
-// OC1A output: Disconnected
-// OC1B output: Disconnected
-// Noise Canceler: Off
-// Input Capture on Falling Edge
-// Timer1 Overflow Interrupt: Off
-// Input Capture Interrupt: Off
-// Compare A Match Interrupt: Off
-// Compare B Match Interrupt: Off
-TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
-TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
-TCNT1H=0x00;
-TCNT1L=0x00;
-ICR1H=0x00;
-ICR1L=0x00;
-OCR1AH=0x00;
-OCR1AL=0x00;
-OCR1BH=0x00;
-OCR1BL=0x00;
+	// External Interrupt(s) initialization
+	// INT0: Off
+	// INT1: Off
+	// Interrupt on any change on pins PCINT0-7: Off
+	// Interrupt on any change on pins PCINT8-10: Off
+	// Interrupt on any change on pins PCINT11-17: Off
+	MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
+	GIMSK=(0<<INT1) | (0<<INT0) | (0<<PCIE0) | (0<<PCIE2) | (0<<PCIE1);
 
-// Timer(s)/Counter(s) Interrupt(s) initialization
-TIMSK=(0<<TOIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<ICIE1) | (0<<OCIE0B) | (0<<TOIE0) | (0<<OCIE0A);
+	// USI initialization
+	// Mode: Disabled
+	// Clock source: Register & Counter=no clk.
+	// USI Counter Overflow Interrupt: Off
+	USICR=(0<<USISIE) | (0<<USIOIE) | (0<<USIWM1) | (0<<USIWM0) | (0<<USICS1) | (0<<USICS0) | (0<<USICLK) | (0<<USITC);
 
-// External Interrupt(s) initialization
-// INT0: Off
-// INT1: Off
-// Interrupt on any change on pins PCINT0-7: Off
-// Interrupt on any change on pins PCINT8-10: Off
-// Interrupt on any change on pins PCINT11-17: Off
-MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
-GIMSK=(0<<INT1) | (0<<INT0) | (0<<PCIE0) | (0<<PCIE2) | (0<<PCIE1);
+	// USART initialization
+	// USART disabled
+	UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
 
-// USI initialization
-// Mode: Disabled
-// Clock source: Register & Counter=no clk.
-// USI Counter Overflow Interrupt: Off
-USICR=(0<<USISIE) | (0<<USIOIE) | (0<<USIWM1) | (0<<USIWM0) | (0<<USICS1) | (0<<USICS0) | (0<<USICLK) | (0<<USITC);
+	// Analog Comparator initialization
+	// Analog Comparator: Off
+	// The Analog Comparator's positive input is
+	// connected to the AIN0 pin
+	// The Analog Comparator's negative input is
+	// connected to the AIN1 pin
+	ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+	// Digital input buffer on AIN0: On
+	// Digital input buffer on AIN1: On
+	DIDR=(0<<AIN0D) | (0<<AIN1D);
 
-// USART initialization
-// USART disabled
-UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+	for (i=1;i>0;i++) {
+		if (PIND.5==0) break;
+		if (i==0) {rab=0b0000001; PORTB=rab|0b00010000; continue;}
+		if (i==1) {rab=0b0000011; PORTB=rab|0b00010000; continue;}
+		rab=rol(rab);
+		PORTB=rab|0b00010000;
+	delay_ms(10);}
 
-// Analog Comparator initialization
-// Analog Comparator: Off
-// The Analog Comparator's positive input is
-// connected to the AIN0 pin
-// The Analog Comparator's negative input is
-// connected to the AIN1 pin
-ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
-// Digital input buffer on AIN0: On
-// Digital input buffer on AIN1: On
-DIDR=(0<<AIN0D) | (0<<AIN1D);
-
-//Возвращаем диск в стартовое положение;
-
-/*while (PIND.5!=1)
-      {
-      PORTB=rab;
-      rab=rab<<1;
-      if (rab==0b00010000) rab=0b00000001;
-      delay_ms(1000);
-      }*/
-      
-while (1)
-      {
-      // Place your code here
-      if (PIND.2!=0 || PIND.3!=0 || PIND.4!=0) {
-      mark=(PIND.4<<2|PIND.3<<1|PIND.2)&0b00000111;
-      step_r=mark-pos;
-      step_l=6-mark+pos;
-        if (step_r<=step_l) {
-        while (step_r) {  
-            for (i=0;i<=20;i++) {
-              PORTB=rab;
-              rab=rab<<1;
-              if (rab==0b00010000) rab=0b00000001;
-              delay_ms(1000);
-            };
-           step_r--;
-           };
-        }
-        else  {
-        while (step_l) {  
-            for (i=0;i<=20;i++) {
-              PORTB=rab;
-              rab=rab<<1;
-              if (rab==0b00010000) rab=0b00000001;
-              delay_ms(1000);
-              };
-           step_l--;
-           };
-            
-        };
-      }
-      }
+	while (1)
+	{
+		mark=pos;
+		if (PIND.2==0 || PIND.3==0 || PIND.4==0) {
+			mark=0;
+			delay_ms(2);
+			mark=(!PIND.4<<2|!PIND.3<<1|!PIND.2)&0b111;
+			while (PIND.2==0 || PIND.3==0 || PIND.4==0) {}
+			delay_ms(2);
+		}
+		while (pos != mark) {
+			for (i=0;i<160;i++) {
+				if (i==0) {rab=0b0000001; PORTB=rab|0b00010000; continue;}
+				if (i==1) {rab=0b0000011; PORTB=rab|0b00010000; continue;}
+				rab=rol(rab);
+				PORTB=rab|0b00010000;
+				delay_ms(10);
+			};
+			pos=pos+1;
+			if (PIND.5==0 || pos==7)
+			pos = 1;
+		};
+	};
 }
